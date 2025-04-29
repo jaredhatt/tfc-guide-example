ffec792bb121c0678f08dcb057ea63b1cd0ee1ed
jaredhatt <jared.hatt@gmail.com>
2025-04-30 11:58:01 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "region" {
   default     = "us-west-1"
 }
 
+variable "test" {
+  default "test"
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
