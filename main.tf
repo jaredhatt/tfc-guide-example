c41b4b76cf4d8f7b17e512a8c0226b795f66021b
jaredhatt <jared.hatt@gmail.com>
2025-04-30 11:50:32 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,19 @@
 
 provider "aws" {
   region = var.region
+    token = var.AWS_SESSION_TOKEN
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+
+}
+variable "AWS_SESSION_TOKEN" {
+  
+}
+variable "AWS_ACCESS_KEY_ID" {
+  
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  
 }
 
 data "aws_ami" "ubuntu" {
